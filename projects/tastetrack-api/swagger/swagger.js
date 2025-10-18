23823b99f1df549d74a45a06799b1e607d168e10
chiomaezekwe <chummyankie@gmail.com>
2025-10-19 00:19:46 +0100

    changes made to swagger

--- projects/tastetrack-api/swagger/swagger.js
+++ projects/tastetrack-api/swagger/swagger.js
@@ -9,7 +9,7 @@ const options = {
       version: '1.0.0',
     },
     servers: [
-      { url: 'http://localhost:3000' }
+      { url: 'https://cse341-tastetrack-api.onrender.com' }  //http://localhost:3000'
     ],
 
     components: {
